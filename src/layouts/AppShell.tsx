7363226135e8e42ab389afee1190fa7f5777251f
ocean <oceanwangyang@163.com>
2026-01-02 05:39:35 +0800

    Fix toolbar inset on fullscreen

--- src/layouts/AppShell.tsx
+++ src/layouts/AppShell.tsx
@@ -1,9 +1,17 @@
 import type { JSX } from "solid-js";
+import { createEffect } from "solid-js";
+import { useWindowFullscreen } from "../state/useWindowFullscreen";
 
 export type AppShellProps = {
   children: JSX.Element;
 };
 
 export const AppShell = (props: AppShellProps) => {
+  const isFullscreen = useWindowFullscreen();
+
+  createEffect(() => {
+    document.documentElement.dataset.windowFullscreen = isFullscreen() ? "true" : "false";
+  });
+
   return <div class="app-shell">{props.children}</div>;
 };
